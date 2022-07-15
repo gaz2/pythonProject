b2863a100cf2fef3e6b9dab811435a80e911e839
gzrch2 <slavenbar@mail.ru>
2022-07-15 18:33:31 +0300

    redirect-links

--- venv/file_fo_git.py
+++ venv/file_fo_git.py
@@ -1,2 +1,17 @@
-print("Hello Slava!")
-print('Я отправил файл в Гит!')
+import requests
+import sys
+import urllib3
+import certifi
+
+http = urllib3.PoolManager(ca_certs=certifi.where())
+url = 'https://httpbin.org/'
+payloads = ['redirect-to?url=/','redirect_to?url=/']
+for payload in payloads:
+    resp = http.request('GET', url+payload, redirect=True)
+    #req = requests.get(url+payload)
+    if payload not in resp:
+        print(payload,resp.status)
+        #print(resp.geturl())
+        #print(resp.info())
+    else:
+        print('Mot redirect')
